9e3607ff180762e4b9a1b9453b98da405ede81be
volodymyr <volodymyrklms@gmail.com>
2021-03-12 14:35:16 +0200

    sync buttons

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import { CategoriesV2Service } from '../shared/categories-v2.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MenuPage implements OnInit {
 
